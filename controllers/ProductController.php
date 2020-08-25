708e6bbf05b8ff9a2e366ce51692bf73b310a6a9
Dat Nguyen <dat.nguyen.d16ptit@gmail.com>
2020-08-25 19:11:37 +0700

    fix validate file and text unicode

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -21,36 +21,53 @@
 		}
 
 		public function find($errors = []) {
-			if (User::findByName($_SESSION['username'])->id == Product::find($_GET['id'])->getUserId()) {
-				if(isset($_GET['id']) || isset($_POST['id'])) {
-					if(isset($_GET['id'])) {
-						$id = $_GET['id'];
-					} else if(isset($_POST['id'])) {
-						$id = $_POST['id'];
-					}
-					$product = Product::find($id);
-					$data = [
-						'product' => $product,
-						'errors'  => $errors
-					];
-					$this->render('EditProductPage', $data);
+			if(isset($_GET['id']) || isset($_POST['id'])) {
+				if(isset($_GET['id'])) {
+					$id = $_GET['id'];
+				} else if(isset($_POST['id'])) {
+					$id = $_POST['id'];
 				}
+				$product = Product::find($id);
+			}
+			if (User::findByName($_SESSION['username'])->id == $product->getUserId()) {
+				$data = [
+					'product' => $product,
+					'errors'  => $errors
+				];
+				$this->render('EditProductPage', $data);
 			} else {
-				die("Unauthorize Action.");
+				die("Không thể sửa sản phẩm này.");
 			}
 		}
 
 		public function edit() {
-			if(isset($_POST['edit-product'])) {
+			if(isset($_POST['edit-product']) && isset($_FILES['edit-image'])) {
 				$errors = $this->checkValidateInput();
-				if (isset($_FILES['edit-image']) && $_FILES['edit-image']['error']) {
-					$image_link = $_POST['image'];
-				} else {
-					$image_link = BASE_URL . '/assets/images/'. $_FILES['edit-image']['name'];
+
+				$destination = 'assets/images/' . basename($_FILES['edit-image']['name']);
+				$imageFileType = pathinfo($destination, PATHINFO_EXTENSION);
+				$types = ['jpg', 'jpeg', 'png'];
+				
+				if($_FILES['edit-image']['error'] !== 4) {
+					if(!getimagesize($_FILES['edit-image']['tmp_name'])) {
+						$errors['image'] = "File này không phải là ảnh.";
+					}
+					else if(!in_array($imageFileType, $types)) {
+						$errors['image'] = "Chỉ chấp nhận ảnh có định dạng jpg, jpeg và png.";
+					}
+					else if(!move_uploaded_file($_FILES['edit-image']['tmp_name'], $destination)) {
+						$errors['image'] = "Ảnh chưa được hệ thống lưu lại.";
+					}
 				}
+				
 				if(!empty($errors)) {
 					$this->find($errors);
 				} else {
+					if($_FILES['edit-image']['error'] == 4) {
+						$image_link = $_POST['image'];
+					} else {
+						$image_link = BASE_URL . '/assets/images/'. $_FILES['edit-image']['name'];
+					}
 					Product::edit(
 						$_POST['id'],
 						$_POST['name'],
@@ -72,10 +89,28 @@
 		}
 
 		public function create() {
-			if(isset($_POST['create-product'])) {
+			if(isset($_POST['create-product']) && isset($_FILES['new-image'])) {
 				$errors = $this->checkValidateInput();
-				if(isset($_FILES['new-image']) && $_FILES['new-image']['error']) {
-					$errors['image'] = "File lỗi.";
+
+				$destination = 'assets/images/' . basename($_FILES['new-image']['name']);
+				$imageFileType = pathinfo($destination, PATHINFO_EXTENSION);
+				$types = ['jpg', 'jpeg', 'png'];
+
+				if($_FILES['new-image']['error']){
+					if($_FILES['new-image']['error'] == 4) {
+						$errors['image'] = "Chưa chọn file ảnh.";
+					} else {
+						$errors['image'] = "File lỗi, mã lỗi là " . $_FILES['new-image']['error'];
+					}
+				}
+				else if(!getimagesize($_FILES['new-image']['tmp_name'])) {
+					$errors['image'] = "File này không phải là ảnh.";
+				}
+				else if(!in_array($imageFileType, $types)) {
+					$errors['image'] = "Chỉ chấp nhận ảnh có định dạng jpg, jpeg và png.";
+				}
+				else if(!move_uploaded_file($_FILES['new-image']['tmp_name'], $destination)) {
+					$errors['image'] = "Ảnh chưa được hệ thống lưu lại.";
 				}
 
 				if(!empty($errors)) {
@@ -103,16 +138,23 @@
 
 		public function checkValidateInput() {
 			$errors = [];
+			// $pattern = "/^[a-zA-Z0-9 .]$/";
+			$pattern = "/^[aAàÀảẢãÃáÁạẠăĂằẰẳẲẵẴắẮặẶâÂầẦẩẨẫẪấẤậẬbBcCdDđĐeEèÈẻẺẽẼéÉẹẸêÊềỀểỂễỄếẾệỆfFgGhHiIìÌỉỈĩĨíÍịỊjJkKlLmMnNoOòÒỏỎõÕóÓọỌôÔồỒổỔỗỖốỐộỘơƠờỜởỞỡỠớỚợỢpPqQrRsStTuUùÙủỦũŨúÚụỤưƯừỪửỬữỮứỨựỰvVwWxXyYỳỲỷỶỹỸýÝỵỴzZ0-9 .]+$/";
 			foreach ($_POST as $key => $value) {
 				if($value == null) {
 					$errors[$key] = "Không được bỏ trống.";
 				}
-				else if($key == 'price' && !filter_var($value, FILTER_VALIDATE_INT)) {
-					$errors[$key] = "Giá bán phải là số nguyên.";
+				else {
+					if($key == 'price' && !filter_var($value, FILTER_VALIDATE_INT)) {
+						$errors[$key] = "Giá bán phải là số nguyên.";
+					}
+					if($key != 'price' && $key != 'image' && !preg_match($pattern, $value)) {
+						$errors[$key] = "Không hợp lệ.";
+					}
 				}
+				
 			}
 			return $errors;
 		}
-
 	}
 ?>
